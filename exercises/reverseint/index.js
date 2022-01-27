0fd3a6407376d703296a0f46f1251d6256341616
Matt Roberts <matt.roberts@getstream.io>
2022-01-27 08:41:22 -0700

    reverse int solution

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,11 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  // make a string then reverse it
+  const reversed = n.toString().split('').reverse().join('');
+  // turn to number and take care of neg sign(-)
+  return parseInt(reversed) * Math.sign(n);
+}
 
 module.exports = reverseInt;
