b462c3b4ea4d87ddb51fe5b2cfa15494b71b29b9
Matt Roberts <justskiit@gmail.com>
2022-02-05 16:27:34 -0700

    anagrams and array chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,38 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  // create empty chunked array
+  const chunked = [];
+  let index = 0;
 
+  while(index < array.length){
+    chunked.push(array.slice(index, index + size));
+    index += size;
+  }
+  return chunked;
+}
+// chunk([1, 2, 3, 4], 2)
 module.exports = chunk;
+
+
+// function chunk(array, size) {
+//   // need an array container to push results to
+//   const chunked = [];
+//   // iterate through array
+//   for( let element of array){
+//     // retrieve last element in chunked
+//     const last = chunked[chunked.length - 1];
+//     // if last element does not exist, or it's length is === to chunk size
+//     if(!last || last.length === size) {
+//       // push a new chunk into chunked with the current element
+//       chunked.push([element]);
+//     } else {
+//     // else add the current element into the chunk
+//       last.push(element);
+//     }
+//   }
+//   // return the new array with sub arrays
+//   return chunked;
+//   // console.log(newArr);
+// }
