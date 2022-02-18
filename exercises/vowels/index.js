5b2418f9b98dbd87ba63e8f5846dbb11ca122d56
Matt Roberts <justskiit@gmail.com>
2022-02-18 04:50:27 -0700

    vowels two solutions

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,26 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  const matches = str.match(/[aeiou]/gi);
+  return matches ? matches.length : 0;
+}
 
 module.exports = vowels;
+
+
+// function vowels(str) {
+//   // create array of vowels to compare agains
+//   const vowelArr = ['a', 'e', 'i', 'o', 'u'];
+//   // create result variable to return
+//   let result = 0;
+//   // to compare everything should be lower case
+//   // iterate over string
+//   for( const char of str.toLowerCase()){
+//     if(vowelArr.includes(char)){
+//       result++;
+//     }
+//   }
+
+//   return result;
+// }
