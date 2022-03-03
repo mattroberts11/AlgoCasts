b26c131d293c8a7c96f3d774f1b161fcb9140573
Matt Roberts <justskiit@gmail.com>
2022-03-03 08:26:57 -0700

    Binary search trees and javascript events

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -3,18 +3,61 @@
 
 function bubbleSort(arr) {
 
+  for(let i = 0; i < arr.length; i++){
+    for(let j = 0; j < (arr.length - i - 1); j++){
+      if(arr[j] > arr[j + 1]){
+        // swap elements
+        const lesser = arr[j + 1];
+        arr[j + 1] = arr[j];
+        arr[j] = lesser;
+      }
+    }
+  }
+  return arr;
 }
 
-function selectionSort(arr) {
-
+// [100, -40, 500, -124, 0, 21, 7]
+function selectionSort(arr) {  // prove me wrong sort by always assuming i is the least in the array
+  for(let i = 0; i < arr.length; i++){
+    // assume i is least in the array assign i to indexMin
+    let indexOfMin = i;
+    for(let j = i + 1; j < arr.length; j++){
+      if(arr[j] < arr[indexOfMin]){
+        indexOfMin = j;
+      }
+    }
+    if(indexOfMin !== i){
+      let lesser = arr[indexOfMin];
+      arr[indexOfMin] = arr[i];
+      arr[i] = lesser;
+    }
+  }
+  return arr;
 }
 
-function mergeSort(arr) {
+function mergeSort(arr) { // takes array splits into two and merges together
+  if(arr.length === 1){
+    return arr;
+  }
 
+  const center = Math.floor(arr.length / 2);
+  const left = arr.slice(0, center); // up to and not including center index
+  const right = arr.slice(center);
+
+
+  return merge(mergeSort(left), mergeSort(right));
 }
 
-function merge(left, right) {
-
+function merge(left, right) { // take two sorted arrays and move them into one array
+  const results = [];
+  while( left.length && right.length){
+    if(left[0] < right[0]){
+      results.push(left.shift());
+    } else {
+      results.push(right.shift());
+    }
+  }
+  return [...results, ...left, ...right];// same as using concat()
 }
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
