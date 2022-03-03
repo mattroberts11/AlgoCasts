6e1a59dbafdeae95140cb10bfbc59bc0b30f1274
Matt Roberts <justskiit@gmail.com>
2022-03-03 09:16:18 -0700

    Pyramid on my own

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,27 +14,56 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {
-  // determine how many columns there will be
-  let totalCols = n + (n - 1);
-  let midpoint = Math.floor(totalCols / 2);
-
-  for(let row = 0; row < n; row++) {
+function pyramid(n) { // n is number of rows
+ // determine number of cols
+  const numCols = n + (n - 1);
+  // deterimine midpoint of columns
+  const midpoint = Math.floor(numCols / 2);
+ // build the row
+  for(let row = 0; row < n; row++){
+    // build level
     let level = '';
-
-    for(let col = 0; col < totalCols; col++){
-      // if the col should have a #
-      if( midpoint - row <= col && midpoint + row >= col) {
-        // add # to level
+    // build cols
+    for(let col = 0; col < numCols; col++){
+      // if to add a space or #
+      if(midpoint - row <= col && midpoint + row >= col){
         level += '#';
       } else {
-      // else add a space to level
         level += ' ';
       }
     }
-    // console log level
     console.log(level);
   }
+
+
 }
-// pyramid(4) // for debug
-module.exports = pyramid;
+pyramid(3) // for debug
+// module.exports = pyramid;
+
+
+
+
+
+
+// function pyramid(n) {
+//   // determine how many columns there will be
+//   let totalCols = n + (n - 1);
+//   let midpoint = Math.floor(totalCols / 2);
+
+//   for(let row = 0; row < n; row++) {
+//     let level = '';
+
+//     for(let col = 0; col < totalCols; col++){
+//       // if the col should have a #
+//       if( midpoint - row <= col && midpoint + row >= col) {
+//         // add # to level
+//         level += '#';
+//       } else {
+//       // else add a space to level
+//         level += ' ';
+//       }
+//     }
+//     // console log level
+//     console.log(level);
+//   }
+// }
